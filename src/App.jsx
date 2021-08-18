da2ea5adf596c8e3addf6602be04b8e125b74d75
O.M. Miles <73572370+ommiles@users.noreply.github.com>
2021-08-18 15:29:59 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+/* 
+  HELP! Our to-do list is broken, 
+  we need someone to debug and fix the following issues:
+
+ 1) The button to add a todo is broken
+ 2) When you add a todo, it replaces all the rest of them
+ 3) The remove todo buttons are broken
+ 4) The input field does not clear after a todo is submitted
+ 5) The todo list should not have duplicates,
+    fix the add todo functionality to not allow duplicate entries.
+*/
+
+const ToDoList = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [todoList, setTodoList] = useState([
+    "Take out trash",
+    "Cook pot roast",
+    "Finish homework"
+  ]);
+
+  const addTodo = (todo) => setTodoList([todo]);
+
+  const removeTodo = () => {};
+
+  return (
+    <div className="todo-list">
+      {todoList.map((todo) => (
+        <div className="todo">
+          {todo}
+          <button onClick={removeTodo}>x</button>
+        </div>
+      ))}
+      <div>
+        <input
+          type="text"
+          value={inputValue}
+          placeholder="Make a new todo!"
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button>+</button>
+      </div>
+    </div>
+  );
+};
+
+export default ToDoList;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom";
+
+import ToDoList from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<ToDoList />, rootElement);
